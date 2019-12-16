886b80b26b9fbbe3e086f0c8494ed69cba3ae0ac
Tero Karttunen <tero.karttunen@cybercom.com>
2019-12-16 10:30:52 +0200

    music improved

--- music_notes.asm
+++ music_notes.asm
@@ -1,154 +1,154 @@
-!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,quarter,5
+!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,quarter,3
 
-!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,4
-!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,4
-!by g_4_l,g_4_h,c_4_l,c_4_h,g_3_l,g_3_h,quarter,5
+!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,2
+!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,2
+!by g_4_l,g_4_h,c_4_l,c_4_h,g_3_l,g_3_h,quarter,3
 !by g_4_l,g_4_h,c_4_l,c_4_h,g_3_l,g_3_h,quarter_dot,8
-!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,4
+!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,2
 
-!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,4
-!by b_4_l,b_4_h,f_2_l,f_2_h,b_4_l,b_4_h,eight,4
-!by c_5_l,c_5_h,c_4_l,c_4_h,a_3_l,a_3_h,quarter,5
+!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,2
+!by b_4_l,b_4_h,f_2_l,f_2_h,b_4_l,b_4_h,eight,2
+!by c_5_l,c_5_h,c_4_l,c_4_h,a_3_l,a_3_h,quarter,3
 !by c_5_l,c_5_h,c_4_l,c_4_h,a_3_l,a_3_h,half,10
 
-!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,4
-!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,4
-!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,5
-!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,5
-!by g_4_l,g_4_h,d_4_l,d_4_h,b_3_bis_l,b_3_bis_h,quarter,5
+!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,2
+!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,2
+!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,3
+!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,3
+!by g_4_l,g_4_h,d_4_l,d_4_h,b_3_bis_l,b_3_bis_h,quarter,3
 
-!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,4
-!by g_4_l,g_4_h,f_2_l,f_2_h,g_4_l,g_4_h,eight,4
-!by f_4_l,f_4_h,a_3_l,a_3_h,f_3_l,f_3_h,quarter,5
+!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,2
+!by g_4_l,g_4_h,f_2_l,f_2_h,g_4_l,g_4_h,eight,2
+!by f_4_l,f_4_h,a_3_l,a_3_h,f_3_l,f_3_h,quarter,3
 !by f_4_l,f_4_h,a_3_l,a_3_h,f_3_l,f_3_h,half,10
 
-!by e_4_l,e_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,5
-!by g_4_l,g_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,5
-!by c_4_l,c_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,5
-!by e_4_l,e_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,5
+!by e_4_l,e_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,3
+!by g_4_l,g_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,3
+!by c_4_l,c_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,3
+!by e_4_l,e_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,3
 
-!by d_4_l,d_4_h,a_3_l,a_3_h,f_3_l,f_3_h,quarter,5
-!by f_4_l,f_4_h,a_3_l,a_3_h,f_3_l,f_3_h,half,10
-!by b_3_l,b_3_h,b_3_l,b_3_h,b_3_l,b_3_h,quarter,5
+!by d_4_l,d_4_h,a_3_l,a_3_h,f_3_l,f_3_h,quarter,3
+!by f_4_l,f_4_h,a_3_l,a_3_h,f_3_l,f_3_h,half,6
+!by b_3_l,b_3_h,b_3_l,b_3_h,b_3_l,b_3_h,quarter,3
 
-!by c_4_l,c_4_h,c_2_l,c_2_h,c_4_l,c_4_h,half,10
-!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,quarter,5
-!by g_4_l,g_4_h,c_3_l,c_3_h,g_4_l,g_4_h,quarter,5
+!by c_4_l,c_4_h,c_2_l,c_2_h,c_4_l,c_4_h,half,12
+!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,quarter,3
+!by g_4_l,g_4_h,c_3_l,c_3_h,g_4_l,g_4_h,quarter,3
 
-!by a_4_l,a_4_h,f_4_l,f_4_h,g_2_l,g_2_h,quarter,5
-!by g_4_l,g_4_h,b_3_l,b_3_h,g_3_l,g_3_h,quarter,5
-!by g_4_l,g_4_h,b_3_l,b_3_h,g_3_l,g_3_h,quarter,5
-!by b_3_l,b_3_h,b_3_l,b_3_h,g_3_l,g_3_h,eight,4
-!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,4
+!by a_4_l,a_4_h,f_4_l,f_4_h,g_2_l,g_2_h,quarter,3
+!by g_4_l,g_4_h,b_3_l,b_3_h,g_3_l,g_3_h,quarter,3
+!by g_4_l,g_4_h,b_3_l,b_3_h,g_3_l,g_3_h,quarter,3
+!by b_3_l,b_3_h,b_3_l,b_3_h,g_3_l,g_3_h,eight,2
+!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,2
 
-!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,4
-!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,4
-!by g_4_l,g_4_h,c_4_l,c_4_h,g_3_l,g_3_h,quarter,5
+!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,2
+!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,2
+!by g_4_l,g_4_h,c_4_l,c_4_h,g_3_l,g_3_h,quarter,3
 !by g_4_l,g_4_h,c_4_l,c_4_h,g_3_l,g_3_h,quarter_dot,8
-!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,4
+!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,2
 
-!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,4
-!by b_4_l,b_4_h,f_2_l,f_2_h,b_4_l,b_4_h,eight,4
-!by c_5_l,c_5_h,c_4_l,c_4_h,a_3_l,a_3_h,quarter,5
+!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,2
+!by b_4_l,b_4_h,f_2_l,f_2_h,b_4_l,b_4_h,eight,2
+!by c_5_l,c_5_h,c_4_l,c_4_h,a_3_l,a_3_h,quarter,3
 !by c_5_l,c_5_h,c_4_l,c_4_h,a_3_l,a_3_h,half,10
 
-!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,4
-!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,4
-!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,5
-!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,5
-!by g_4_l,g_4_h,d_4_l,d_4_h,b_3_bis_l,b_3_bis_h,quarter,5
+!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,2
+!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,2
+!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,3
+!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,3
+!by g_4_l,g_4_h,d_4_l,d_4_h,b_3_bis_l,b_3_bis_h,quarter,3
 
-!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,4
-!by g_4_l,g_4_h,f_2_l,f_2_h,g_4_l,g_4_h,eight,4
-!by f_4_l,f_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,5
+!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,2
+!by g_4_l,g_4_h,f_2_l,f_2_h,g_4_l,g_4_h,eight,2
+!by f_4_l,f_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,3
 !by f_4_l,f_4_h,c_3_l,c_3_h,a_3_l,a_3_h,half,10
 
-!by e_4_l,e_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,5
-!by g_4_l,g_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,5
-!by c_4_l,c_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,5
-!by e_4_l,e_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,5
+!by e_4_l,e_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,3
+!by g_4_l,g_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,3
+!by c_4_l,c_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,3
+!by e_4_l,e_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,3
 
-!by d_4_l,d_4_h,a_3_l,a_3_h,f_3_l,f_3_h,quarter,5
-!by f_4_l,f_4_h,a_3_l,a_3_h,f_3_l,f_3_h,half,10
-!by b_3_l,b_3_h,b_3_l,b_3_h,b_3_l,b_3_h,quarter,5
+!by d_4_l,d_4_h,a_3_l,a_3_h,f_3_l,f_3_h,quarter,3
+!by f_4_l,f_4_h,a_3_l,a_3_h,f_3_l,f_3_h,half,6
+!by b_3_l,b_3_h,b_3_l,b_3_h,b_3_l,b_3_h,quarter,3
 
 !by c_4_l,c_4_h,c_2_l,c_2_h,c_4_l,c_4_h,whole,20
 
-!by g_3_l,g_3_h,e_3_l,e_3_h,c_3_l,c_3_h,quarter,5
-!by g_3_l,g_3_h,e_3_l,e_3_h,c_3_l,c_3_h,quarter,5
-!by g_3_l,g_3_h,e_3_l,e_3_h,c_3_l,c_3_h,quarter,5
-!by c_5_l,c_5_h,c_5_l,c_5_h,c_5_l,c_5_h,quarter,5
+!by g_3_l,g_3_h,e_3_l,e_3_h,c_3_l,c_3_h,quarter,3
+!by g_3_l,g_3_h,e_3_l,e_3_h,c_3_l,c_3_h,quarter,3
+!by g_3_l,g_3_h,e_3_l,e_3_h,c_3_l,c_3_h,quarter,3
+!by c_5_l,c_5_h,c_5_l,c_5_h,c_5_l,c_5_h,quarter,3
 
-!by d_5_l,d_5_h,c_3_l,c_3_h,d_5_l,d_5_h,quarter,5
-!by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
-!by b_4_l,b_4_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
-!by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
+!by d_5_l,d_5_h,c_3_l,c_3_h,d_5_l,d_5_h,quarter,3
+!by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,3
+!by b_4_l,b_4_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,3
+!by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,3
 
-!by a_4_l,a_4_h,f_3_l,f_3_h,a_4_l,a_4_h,quarter,5
-!by a_4_l,a_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,5
-!by a_4_l,a_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,5
-!by c_5_l,c_5_h,c_5_l,c_5_h,c_5_l,c_5_h,quarter,5
+!by a_4_l,a_4_h,f_3_l,f_3_h,a_4_l,a_4_h,quarter,3
+!by a_4_l,a_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,3
+!by a_4_l,a_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,3
+!by c_5_l,c_5_h,c_5_l,c_5_h,c_5_l,c_5_h,quarter,3
 
-!by d_5_l,d_5_h,c_3_l,c_3_h,d_5_l,d_5_h,quarter,5
-!by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
-!by b_4_l,b_4_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
-!by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
+!by d_5_l,d_5_h,c_3_l,c_3_h,d_5_l,d_5_h,quarter,3
+!by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,3
+!by b_4_l,b_4_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,3
+!by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,3
 
-!by a_4_l,a_4_h,f_3_l,f_3_h,a_4_l,a_4_h,quarter,5
-!by a_4_l,a_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,5
-!by a_4_l,a_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,5
-!by d_5_l,d_5_h,d_5_l,d_5_h,d_5_l,d_5_h,quarter,5
+!by a_4_l,a_4_h,f_3_l,f_3_h,a_4_l,a_4_h,quarter,3
+!by a_4_l,a_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,3
+!by a_4_l,a_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,3
+!by d_5_l,d_5_h,d_5_l,d_5_h,d_5_l,d_5_h,quarter,3
 
-!by e_5_l,e_5_h,g_3_bis_l,g_3_bis_h,e_5_l,e_5_h,quarter,5
-!by d_5_l,d_5_h,d_4_l,d_4_h,c_4_l,c_4_h,quarter,5
-!by d_5_bis_l,d_5_bis_h,d_4_l,d_4_h,c_4_l,c_4_h,quarter,5
-!by d_5_l,d_5_h,d_4_l,d_4_h,c_4_l,c_4_h,quarter,5
+!by e_5_l,e_5_h,g_3_bis_l,g_3_bis_h,e_5_l,e_5_h,quarter,3
+!by d_5_l,d_5_h,d_4_l,d_4_h,c_4_l,c_4_h,quarter,3
+!by d_5_bis_l,d_5_bis_h,d_4_l,d_4_h,c_4_l,c_4_h,quarter,3
+!by d_5_l,d_5_h,d_4_l,d_4_h,c_4_l,c_4_h,quarter,3
 
-!by b_4_l,b_4_h,g_3_l,g_3_h,b_4_l,b_4_h,quarter,5
-!by b_4_l,b_4_h,d_4_l,d_4_h,b_3_l,b_3_h,quarter,5
-!by b_4_l,b_4_h,d_4_l,d_4_h,b_3_l,b_3_h,quarter,5
-!by b_4_l,b_4_h,d_4_l,d_4_h,b_3_l,b_3_h,eight,4
-!by c_5_l,c_5_h,d_4_l,d_4_h,b_3_l,b_3_h,eight,4
+!by b_4_l,b_4_h,g_3_l,g_3_h,b_4_l,b_4_h,quarter,3
+!by b_4_l,b_4_h,d_4_l,d_4_h,b_3_l,b_3_h,quarter,3
+!by b_4_l,b_4_h,d_4_l,d_4_h,b_3_l,b_3_h,quarter,3
+!by b_4_l,b_4_h,d_4_l,d_4_h,b_3_l,b_3_h,eight,2
+!by c_5_l,c_5_h,d_4_l,d_4_h,b_3_l,b_3_h,eight,2
 
-!by d_5_l,d_5_h,d_4_l,d_4_h,c_4_l,c_4_h,quarter,5
-!by c_5_l,c_5_h,d_4_l,d_4_h,c_4_l,c_4_h,quarter,5
-!by b_4_l,b_4_h,b_4_l,b_4_h,b_4_l,b_4_h,quarter,5
-!by a_4_l,a_4_h,a_4_l,a_4_h,a_4_l,a_4_h,quarter,5
+!by d_5_l,d_5_h,d_4_l,d_4_h,c_4_l,c_4_h,quarter,3
+!by c_5_l,c_5_h,d_4_l,d_4_h,c_4_l,c_4_h,quarter,3
+!by b_4_l,b_4_h,b_4_l,b_4_h,b_4_l,b_4_h,quarter,3
+!by a_4_l,a_4_h,a_4_l,a_4_h,a_4_l,a_4_h,quarter,3
 
 !by g_4_l,g_4_h,d_4_l,d_4_h,b_3_l,b_3_h,quarter,30
-!by g_4_l,g_4_h,e_4_bis_l,e_4_bis_h,b_3_l,b_3_h,quarter_dot,8
-!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,4
+!by g_4_l,g_4_h,e_4_bis_l,e_4_bis_h,b_3_l,b_3_h,quarter_dot,4
+!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,2
 
-!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,4
-!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,4
-!by g_4_l,g_4_h,c_4_l,c_4_h,g_3_l,g_3_h,quarter,5
+!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,2
+!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,2
+!by g_4_l,g_4_h,c_4_l,c_4_h,g_3_l,g_3_h,quarter,3
 !by g_4_l,g_4_h,c_4_l,c_4_h,g_3_l,g_3_h,quarter_dot,8
-!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,4
+!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,2
 
-!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,4
-!by b_4_l,b_4_h,f_2_l,f_2_h,b_4_l,b_4_h,eight,4
-!by c_5_l,c_5_h,c_4_l,c_4_h,a_3_l,a_3_h,quarter,5
+!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,2
+!by b_4_l,b_4_h,f_2_l,f_2_h,b_4_l,b_4_h,eight,2
+!by c_5_l,c_5_h,c_4_l,c_4_h,a_3_l,a_3_h,quarter,3
 !by c_5_l,c_5_h,c_4_l,c_4_h,a_3_l,a_3_h,half,10
 
-!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,4
-!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,4
-!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,5
-!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,5
-!by g_4_l,g_4_h,d_4_l,d_4_h,b_3_bis_l,b_3_bis_h,quarter,5
+!by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,2
+!by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,2
+!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,3
+!by g_4_l,g_4_h,d_4_l,d_4_h,g_3_l,g_3_h,quarter,3
+!by g_4_l,g_4_h,d_4_l,d_4_h,b_3_bis_l,b_3_bis_h,quarter,3
 
-!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,4
-!by g_4_l,g_4_h,f_2_l,f_2_h,g_4_l,g_4_h,eight,4
-!by f_4_l,f_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,5
+!by a_4_l,a_4_h,f_2_l,f_2_h,a_4_l,a_4_h,eight,2
+!by g_4_l,g_4_h,f_2_l,f_2_h,g_4_l,g_4_h,eight,2
+!by f_4_l,f_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,3
 !by f_4_l,f_4_h,c_3_l,c_3_h,a_3_l,a_3_h,half,10
 
-!by e_4_l,e_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,5
-!by g_4_l,g_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,5
-!by c_4_l,c_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,5
-!by e_4_l,e_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,5
+!by e_4_l,e_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,3
+!by g_4_l,g_4_h,c_3_l,c_3_h,g_3_l,g_3_h,quarter,3
+!by c_4_l,c_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,3
+!by e_4_l,e_4_h,a_3_l,a_3_h,e_3_l,e_3_h,quarter,3
 
-!by d_4_l,d_4_h,a_3_l,a_3_h,f_3_l,f_3_h,quarter,5
-!by f_4_l,f_4_h,a_3_l,a_3_h,f_3_l,f_3_h,half,10
-!by d_5_l,d_5_h,d_5_l,d_5_h,d_5_l,d_5_h,quarter,5
+!by d_4_l,d_4_h,a_3_l,a_3_h,f_3_l,f_3_h,quarter,3
+!by f_4_l,f_4_h,a_3_l,a_3_h,f_3_l,f_3_h,half,6
+!by d_5_l,d_5_h,d_5_l,d_5_h,d_5_l,d_5_h,quarter,3
 
 !by c_5_l,c_5_h,c_4_l,c_4_h,f_3_l,f_3_h,whole,60
 
