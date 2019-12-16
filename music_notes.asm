2fe5b40d9690d7ffd962b416d7884ca09583d2b6
Tero Karttunen <tero.karttunen@cybercom.com>
2019-12-16 09:54:42 +0200

    music improved

--- music_notes.asm
+++ music_notes.asm
@@ -82,7 +82,7 @@
 !by d_5_l,d_5_h,c_3_l,c_3_h,d_5_l,d_5_h,quarter,5
 !by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
 !by b_4_l,b_4_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
-!by c_5_l,c_5_h,b_3_l,b_3_h,e_3_l,e_3_h,quarter,5
+!by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
 
 !by a_4_l,a_4_h,f_3_l,f_3_h,a_4_l,a_4_h,quarter,5
 !by a_4_l,a_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,5
@@ -92,7 +92,7 @@
 !by d_5_l,d_5_h,c_3_l,c_3_h,d_5_l,d_5_h,quarter,5
 !by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
 !by b_4_l,b_4_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
-!by c_5_l,c_5_h,b_3_l,b_3_h,e_3_l,e_3_h,quarter,5
+!by c_5_l,c_5_h,b_3_bis_l,b_3_bis_h,e_3_l,e_3_h,quarter,5
 
 !by a_4_l,a_4_h,f_3_l,f_3_h,a_4_l,a_4_h,quarter,5
 !by a_4_l,a_4_h,c_3_l,c_3_h,a_3_l,a_3_h,quarter,5
@@ -115,9 +115,9 @@
 !by b_4_l,b_4_h,b_4_l,b_4_h,b_4_l,b_4_h,quarter,5
 !by a_4_l,a_4_h,a_4_l,a_4_h,a_4_l,a_4_h,quarter,5
 
-!by g_4_l,g_4_h,d_4_l,d_4_h,b_3_l,b_3_h,quarter,40
-!by g_4_l,g_4_h,e_4_bis_l,e_4_bis_h,b_3_l,b_3_h,quarter_dot,40
-!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,40
+!by g_4_l,g_4_h,d_4_l,d_4_h,b_3_l,b_3_h,quarter,30
+!by g_4_l,g_4_h,e_4_bis_l,e_4_bis_h,b_3_l,b_3_h,quarter_dot,8
+!by g_4_l,g_4_h,g_4_l,g_4_h,g_4_l,g_4_h,eight,4
 
 !by e_4_l,e_4_h,c_3_l,c_3_h,e_4_l,e_4_h,eight,4
 !by f_4_l,f_4_h,c_3_l,c_3_h,f_4_l,f_4_h,eight,4
@@ -150,5 +150,5 @@
 !by f_4_l,f_4_h,a_3_l,a_3_h,f_3_l,f_3_h,half,10
 !by d_5_l,d_5_h,d_5_l,d_5_h,d_5_l,d_5_h,quarter,5
 
-!by c_5_l,c_5_h,c_4_l,c_4_h,f_3_l,f_3_h,whole,50
+!by c_5_l,c_5_h,c_4_l,c_4_h,f_3_l,f_3_h,whole,60
 
